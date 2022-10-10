c29d5b53ee181f65a99a806ddab15d5bb32c7c19
wyvernk <30991653+wyvernk@users.noreply.github.com>
2022-10-10 17:21:33 +0800

    merged codes
    removed no hotel with no address on csv. integrated sentimental analysis to do rating reviews

--- UITest.py
+++ UITest.py
@@ -1,9 +1,12 @@
 from tkinter import *
 import math
+import numpy
 import pandas as pd
 import csv
 import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import os.path
+import filtering.py
 
 ##########Initialise values############
 window = Tk()
@@ -21,34 +24,23 @@ HotelMenuList=[]
 #Hotels List
 HotelsList =[]
 #Recommended Hotels List
-RecommendedHotelsList =[]
+RecommendedHotelsList=[]
+#Bookmarked List
+BookmarkedHotelsObjectsList=[]
+#Bookmarked name list
+BookmarkedHotelsNameList=[]
 
-class Hotel:
-    def __init__(self, name, address, scoreslist, reviewslist):
-        self.Name = name
-        self.Address = address
-        self.ScoresList = scoreslist
-        self.ReviewsList = reviewslist
-        TotalScore = 0
-        for rating in ReviewsRating:
-            TotalScore += int(rating)
-
-        self.AverageScore = TotalScore / len(ReviewsRating)
-
-        analysis ={}
-        analysis["Positive"] = [senti.polarity_scores(i)["pos"] for i in self.ReviewsList]
-        analysis["Negative"] = [senti.polarity_scores(i)["neg"] for i in self.ReviewsList]
-        self.Sentiments = analysis
+#for writing bookmarks
+Bookmarkdata=None
+BookmarkDataFrame=None
 
 #dropdown box options
-HotelOptions=[
-    'Hotel Russo Palace',
-    'Little Paradise Hotel',
-    'Hawthorn Suites By Wyndham Livermore Wine Country',
-    'Fairfield Inn By Marriott Binghamton',
-    'Days Inn El Reno Ok',
-    'Polynesian Plaza'
-]
+HotelOptions=[]
+for name in data["name"]: #add all hotels in csv to dropdown
+    if(pd.isnull(name)):
+       continue
+    if(name not in HotelOptions):
+        HotelOptions.append(name)
 
 SortOptions = [
     'Alphabetical A-Z',
@@ -56,6 +48,50 @@ SortOptions = [
     'Rating (High to Low)',
     'Rating (Low to High)'
     ]
+
+if(os.path.exists("../Bookmarks.csv")):
+    Bookmarkdata = pd.read_csv("../Bookmarks.csv")
+    if(len(HotelOptions) != len(Bookmarkdata)):
+        BookmarkDataFrame = pd.DataFrame(HotelOptions, columns=['name'])
+        BookmarkDataFrame["bookmarked"] = False
+        BookmarkDataFrame.to_csv("../Bookmarks.csv")
+        Bookmarkdata = pd.read_csv("../Bookmarks.csv")
+else:
+    BookmarkDataFrame = pd.DataFrame(HotelOptions, columns=['name'])
+    BookmarkDataFrame["bookmarked"] = False
+    BookmarkDataFrame.to_csv("../Bookmarks.csv")
+    Bookmarkdata = pd.read_csv("../Bookmarks.csv")
+
+
+class Hotel:
+    def __init__(self, name, address, scoreslist, reviewslist, bookmarked):
+        self.Name = name
+        self.Address = address
+        self.ScoresList = scoreslist
+        self.ReviewsList = reviewslist
+
+        analysis ={}
+        analysis["Positive"] = [senti.polarity_scores(i)["pos"] for i in self.ReviewsList]
+        analysis["Negative"] = [senti.polarity_scores(i)["neg"] for i in self.ReviewsList]
+        self.Sentiments = analysis
+        self.Bookmarked = bookmarked
+
+        TotalScore = 0.00
+        if(' ' in self.ScoresList):
+            PositiveList = list(analysis["Positive"])
+            NegativeList = list(analysis["Negative"])
+            for index, values in enumerate(PositiveList):
+                totalratingadded = PositiveList[index] + NegativeList[index]
+                if(totalratingadded !=0):
+                    calculated = round((PositiveList[index]/totalratingadded) * 5)
+                    self.ScoresList[index] = calculated
+                    TotalScore+=calculated
+        else:
+            for rating in self.ScoresList:
+                TotalScore += int(rating)
+
+        self.AverageScore = TotalScore / len(self.ScoresList)
+
 
 #################functions#####################
 #clear current hotel details and formatting
@@ -75,6 +111,30 @@ def ClearHotelDetails():
     ReviewsTextList.clear()
     ReviewsScoreList.clear()
 
+def ToggleBookmark():
+    currenthotelname = HotelNameLabel.cget("text")
+    for hotel in HotelsList:
+        if(hotel.Name == currenthotelname):
+            hotel.Bookmarked = BookmarkedVariable.get()
+            if(hotel.Bookmarked):
+                BookmarkedHotelsNameList.append(hotel.Name)
+                BookmarkedHotelsObjectsList.append(hotel)
+                if ("No Bookmarks") in BookmarkedHotelsNameList:
+                    BookmarkedHotelsNameList.remove("No Bookmarks")
+            else:
+                BookmarkedHotelsNameList.remove(hotel.Name)
+                BookmarkedHotelsObjectsList.remove(hotel)
+                if(len(BookmarkedHotelsNameList)==0):
+                    BookmarkedHotelsNameList.append("No Bookmarks")
+
+            Bookmarkdata.loc[Bookmarkdata.name == hotel.Name, "bookmarked"] = hotel.Bookmarked
+            Bookmarkdata.to_csv("../Bookmarks.csv", index= False)
+
+
+    BookmarkDropdown["menu"].delete(0, 'end')
+    for hotel in BookmarkedHotelsNameList:
+        BookmarkDropdown["menu"].add_command(label= hotel, command=lambda hotel = hotel:SelectFromBookmark(hotel))
+
 def FilterAndSortHotelDetails(sortoption = None):
     for HotelButton in HotelMenuList:
         HotelButton.destroy()
@@ -90,8 +150,7 @@ def FilterAndSortHotelDetails(sortoption = None):
     elif (SortVariable.get() == 'Rating (Low to High)'):
         SortedList = sorted(HotelsList, key=lambda x: x.AverageScore)
 
-
-    if(var1.get() == 0 and var2.get() == 0 and var3.get() == 0 and var4.get() == 0 and var5.get() == 0):
+    if(onestar.get() == 0 and twostar.get() == 0 and threestar.get() == 0 and fourstar.get() == 0 and fivestar.get() == 0):
         for index, hotel in enumerate(SortedList):
             HotelMenuButton = Button(MainMenuFrame, text=hotel.Name + "\n"+hotel.Address + "\n Average Score: %0.2f" % hotel.AverageScore,
                                      command=lambda hotel=hotel: DisplayHotelDetails(hotel), width=80, height=6, font=("Arial", 10), relief=GROOVE)
@@ -99,18 +158,30 @@ def FilterAndSortHotelDetails(sortoption = None):
             HotelMenuList.append(HotelMenuButton)
     else:
         for index, hotel in enumerate(SortedList):
-            if(int(hotel.AverageScore) == var1.get() or int(hotel.AverageScore) == var2.get() or int(hotel.AverageScore) == var3.get() or int(hotel.AverageScore) == var4.get() or int(hotel.AverageScore) == var5.get()):
+            if(int(hotel.AverageScore) == onestar.get() or int(hotel.AverageScore) == twostar.get() or int(hotel.AverageScore) == threestar.get() or int(hotel.AverageScore) == fourstar.get() or int(hotel.AverageScore) == fivestar.get()):
                 HotelMenuButton = Button(MainMenuFrame, text=hotel.Name + "\n"+hotel.Address +"\n Average Score: %0.2f" % hotel.AverageScore, command=lambda hotel=hotel: DisplayHotelDetails(hotel), width=80, height=6, font=("Arial", 10), relief=GROOVE)
                 HotelMenuButton.grid(row=index + 2, column=0, sticky=N, pady=10, padx=10)
                 HotelMenuList.append(HotelMenuButton)
 
+    if(len(HotelMenuList) == 0):
+        Emptylabel.grid(row= 1, column=0, sticky=N, pady=0)
+    else:
+        Emptylabel.grid_forget()
+
     UpdateScrollbar()
+
+def SelectFromBookmark(selectedbookmark):
+    for hotel in BookmarkedHotelsObjectsList:
+        if(hotel.Name == selectedbookmark):
+            DisplayHotelDetails(hotel)
+            break
+    BookmarkNameVariable.set("Bookmarks")
 
 def UpdateRecommendations(hotel):
     for HotelButton in RecommendedHotelsList:
         HotelButton.destroy()
     for index, otherhotel in enumerate(HotelsList):
-        if (round(otherhotel.AverageScore) >= math.ceil(hotel.AverageScore) and otherhotel.Name != hotel.Name):
+        if (math.ceil(otherhotel.AverageScore) >= round(hotel.AverageScore) and otherhotel.Name != hotel.Name):
             RecoHotelMenuButton = Button(RecommendationFrame, text=otherhotel.Name + "\n Average Score: %0.2f" % otherhotel.AverageScore,
                                      command=lambda otherhotel=otherhotel: DisplayHotelDetails(otherhotel), width=26, height=6, font=("Arial", 10), relief=GROOVE, wraplength=200)
             RecoHotelMenuButton.grid(row=index+1, column=0, sticky=N, pady=10, padx= 10)
@@ -141,6 +212,8 @@ def DisplayHotelDetails(hotel):
 
     AddressLabel.config(text=("Address: " + hotel.Address))
     AddressLabel.grid(row=1, column=0, sticky=NW)
+
+    BookmarkedVariable.set(hotel.Bookmarked)
 
     AverageScoreLabel.config(text= "Score: %0.2f"%hotel.AverageScore+ "/5")
     AverageScoreLabel.grid(row=2, column=0, sticky=NW)
@@ -183,7 +256,7 @@ def DisplayHotelDetails(hotel):
     UpdateRecommendations(hotel)
     RecommendationFrame.grid(row=1, column=0, padx=40, sticky=N)
     HotelDetailsFrame.grid(row=1, column=1, sticky=N)
-    MenuButton.grid(row=10, column=0, padx=10, pady=10)
+    MenuButton.grid(row=0, column=0, padx=35, pady=20, sticky=NE)
     UpdateScrollbar()
 
 
@@ -194,9 +267,12 @@ for i in HotelOptions:
     ReviewsRating = list(data.loc[data.name == i, 'reviews.rating'])
     AddressList =  data.loc[data.name == i, 'address']
     Address = AddressList.iat[0]
-    HotelObject = Hotel(i, Address, ReviewsRating, ReviewsList)
+    Bookmarked = Bookmarkdata.loc[Bookmarkdata.name == i, 'bookmarked'].bool()
+    HotelObject = Hotel(i, Address, ReviewsRating, ReviewsList, Bookmarked)
     HotelsList.append(HotelObject)
-
+    if(Bookmarked == True):
+        BookmarkedHotelsObjectsList.append(HotelObject)
+        BookmarkedHotelsNameList.append(HotelObject.Name)
 
 ##########UI LAYOUT##########
 #frame for entire window
@@ -217,10 +293,6 @@ MainCanvas.create_window((0,0), window=ContentFrame, anchor=NW)
 MainCanvas.configure(xscrollcommand=HScrollbar.set, yscrollcommand=VScrollbar.set)
 MainCanvas.pack(side=LEFT, fill=BOTH, expand=1)
 
-#Title Frame
-TitleFrame =  Frame(master=ContentFrame)
-TitleFrame.grid(row=0, column=1)
-
 #Filter Frame
 FilterFrame = Frame(master=ContentFrame, highlightbackground="black", highlightthickness=1, padx=10, pady=5)
 
@@ -235,28 +307,42 @@ HotelDetailsFrame = Frame(master=ContentFrame, highlightbackground="black", high
 
 HotelDescriptionFrame = Frame(master=HotelDetailsFrame)
 HotelDescriptionFrame.grid(row=0, column=0, sticky=NW)
+HotelNameFrame = Frame(master=HotelDescriptionFrame)
+HotelNameFrame.grid(row=0, column=0, sticky=NW)
 HotelReviewFrame =  Frame(master=HotelDetailsFrame)
 HotelReviewFrame.grid(row=1, column=0, sticky=NW)
 
 
 #Title
-AppTitleLabel = Label(TitleFrame, text="HOTELOPOLIS", font=("Arial", 40), anchor=CENTER)
-AppTitleLabel.grid(row=0, column=0)
+AppTitleLabel = Label(ContentFrame, text="HOTELOPOLIS", font=("Arial", 40), anchor=CENTER)
+AppTitleLabel.grid(row=0, column=1)
+
+#filter variables
+restaurant=IntVar()
+jacuzzi=IntVar()
+pool=IntVar()
+gym=IntVar()
+spa=IntVar()
 
 #filter options
 FilterLabel = Label(FilterFrame, text="Filters", font=("Arial", 20))
 AmenitiesLabel = Label(FilterFrame, text="Amenities", font=("Arial", 15))
-PoolFilter = Checkbutton(FilterFrame, text="Pool", variable=0)
+RestaurantFilter = Checkbutton(FilterFrame, text="Restaurant", variable=restaurant)
+JacuzziFilter = Checkbutton(FilterFrame, text="Jacuzzi", variable=jacuzzi)
+PoolFilter = Checkbutton(FilterFrame, text="Pool", variable=pool)
+GymFilter = Checkbutton(FilterFrame, text="Gym", variable=gym)
+SpaFilter = Checkbutton(FilterFrame, text="Gym", variable=spa)
+
 
 #Recommendation Label
 RecommendationLabel = Label(RecommendationFrame, text="You may also like:")
 RecommendationLabel.grid(row=0, column=0)
 
 #Menu Button
-MenuButton = Button(RecommendationFrame, text="Back To Menu", command=DisplayMainMenu,font=("Arial", 10), relief=GROOVE)
+MenuButton = Button(ContentFrame, text="Back To Menu", command=DisplayMainMenu,font=("Arial", 10), relief=GROOVE)
 
 #Hotel name label
-HotelNameLabel = Label(HotelDescriptionFrame, font=("Arial", 30))
+HotelNameLabel = Label(HotelNameFrame, font=("Arial", 30))
 
 #Address label
 AddressLabel = Label(HotelDescriptionFrame, text="",font=("Arial", 15))
@@ -264,27 +350,36 @@ AddressLabel = Label(HotelDescriptionFrame, text="",font=("Arial", 15))
 #average star review label
 AverageScoreLabel = Label(HotelDescriptionFrame, text="",font=("Arial", 15))
 
+#Hotel name label
+Emptylabel = Label(MainMenuFrame,text="No Results", font=("Arial", 20))
+
 RatingLabel = Label(FilterFrame, text="Rating", font=("Arial", 15))
-var1=IntVar()
-var2=IntVar()
-var3=IntVar()
-var4=IntVar()
-var5=IntVar()
-OneStarFilter = Checkbutton(FilterFrame, text="1 Star", variable=var1, onvalue=1, offvalue= 0, command=FilterAndSortHotelDetails)
-TwoStarFilter = Checkbutton(FilterFrame, text="2 Stars", variable=var2, onvalue=2, offvalue= 0, command=FilterAndSortHotelDetails)
-ThreeStarFilter = Checkbutton(FilterFrame, text="3 Stars", variable=var3, onvalue=3, offvalue= 0, command=FilterAndSortHotelDetails)
-FourStarFilter = Checkbutton(FilterFrame, text="4 Stars", variable=var4, onvalue=4, offvalue= 0, command=FilterAndSortHotelDetails)
-FiveStarFilter = Checkbutton(FilterFrame, text="5 Stars", variable=var5, onvalue=5, offvalue= 0, command=FilterAndSortHotelDetails)
+onestar=IntVar()
+twostar=IntVar()
+threestar=IntVar()
+fourstar=IntVar()
+fivestar=IntVar()
+
+OneStarFilter = Checkbutton(FilterFrame, text="1 Star", variable=onestar, onvalue=1, offvalue= 0, command=FilterAndSortHotelDetails)
+TwoStarFilter = Checkbutton(FilterFrame, text="2 Stars", variable=twostar, onvalue=2, offvalue= 0, command=FilterAndSortHotelDetails)
+ThreeStarFilter = Checkbutton(FilterFrame, text="3 Stars", variable=threestar, onvalue=3, offvalue= 0, command=FilterAndSortHotelDetails)
+FourStarFilter = Checkbutton(FilterFrame, text="4 Stars", variable=fourstar, onvalue=4, offvalue= 0, command=FilterAndSortHotelDetails)
+FiveStarFilter = Checkbutton(FilterFrame, text="5 Stars", variable=fivestar, onvalue=5, offvalue= 0, command=FilterAndSortHotelDetails)
 
 FilterLabel.grid(row=0, column=0, sticky=W)
 AmenitiesLabel.grid(row=1, column=0, sticky=W)
-PoolFilter.grid(row=2, column=0, sticky=W)
-RatingLabel.grid(row=4, column=0, sticky=W)
-OneStarFilter.grid(row=5, column=0, sticky=W)
-TwoStarFilter.grid(row=6, column=0, sticky=W)
-ThreeStarFilter.grid(row=7, column=0, sticky=W)
-FourStarFilter.grid(row=8, column=0, sticky=W)
-FiveStarFilter.grid(row=9, column=0, sticky=W)
+RestaurantFilter.grid(row=2, column=0, sticky=W)
+JacuzziFilter.grid(row=3, column=0, sticky=W)
+PoolFilter.grid(row=4, column=0, sticky=W)
+GymFilter.grid(row=5, column=0, sticky=W)
+PoolFilter.grid(row=6, column=0, sticky=W)
+SpaFilter.grid(row=7, column=0, sticky=W)
+RatingLabel.grid(row=8, column=0, sticky=W)
+OneStarFilter.grid(row=9, column=0, sticky=W)
+TwoStarFilter.grid(row=10, column=0, sticky=W)
+ThreeStarFilter.grid(row=11, column=0, sticky=W)
+FourStarFilter.grid(row=12, column=0, sticky=W)
+FiveStarFilter.grid(row=13, column=0, sticky=W)
 
 SortVariable = StringVar()
 SortVariable.set(SortOptions[0]) # default value
@@ -295,6 +390,17 @@ SortLabel = Label(SortFrame, text="Sort by:", font=("Arial", 14))
 SortLabel.grid(row=0, column=0, sticky=NW)
 SortDropdown = OptionMenu(SortFrame, SortVariable, *SortOptions, command=FilterAndSortHotelDetails)
 SortDropdown.grid(row=0, column=1, sticky=NW)
+
+BookmarkNameVariable = StringVar()
+BookmarkNameVariable.set("Bookmarks") # default value
+if (len(BookmarkedHotelsNameList) == 0):
+    BookmarkedHotelsNameList.append("No Bookmarks")
+BookmarkDropdown = OptionMenu(ContentFrame, BookmarkNameVariable, *BookmarkedHotelsNameList, command=SelectFromBookmark)
+BookmarkDropdown.grid(row=0, column=0, sticky=W, padx=40)
+
+BookmarkedVariable = BooleanVar()
+BookmarkedButton = Checkbutton(HotelNameFrame, text="Bookmarked", variable=BookmarkedVariable, onvalue=1, offvalue= 0, command=ToggleBookmark)
+BookmarkedButton.grid(row=0, column=1, sticky=W)
 
 FilterAndSortHotelDetails(SortVariable)
 DisplayMainMenu()

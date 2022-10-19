68f69eec12f59e46b84c722829827cb6c9dab423
Victor <kongvictor99@gmail.com>
2022-10-19 17:30:09 +0800

    code for loading screen

---
+++ loadingscreen.py
@@ -0,0 +1,62 @@
+from tkinter import *
+import math
+import pandas
+import pandas as pd
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import os.path
+from filtering import *
+
+from PIL import ImageTk, Image
+from review_analysis import *
+
+# JFStart
+import os
+import os.path
+import time
+import PIL as p
+import PIL.ImageTk as ptk
+import bs4
+import requests
+from webscraper import *
+from PIL import Image
+# google
+from selenium import webdriver
+
+from webscraper import urlchecker
+from selenium.webdriver.chrome.options import Options
+
+
+#loading screen 
+import tkinter as tk
+from time import sleep
+
+
+import tkinter
+from tkinter import *
+from PIL import Image, ImageTk
+
+root = Tk()
+
+
+#end
+
+from tkinter import *
+from PIL import Image, ImageTk
+
+
+def task():
+    # The window will stay open until this function call ends.
+    sleep(2) # Replace this with the code you want to run
+    root.destroy()
+
+img = ImageTk.PhotoImage(Image.open("GdTTaIrf_400x400.png"))
+panel = Label(root, image = img)
+panel.pack(side = "bottom", fill = "both", expand = "yes")
+root.geometry("400x400+520+150")
+root.resizable(width=True, height=True)
+
+
+root.after(2000, task)
+root.mainloop()
+
+print("Main loop is now over and we can do other stuff.")
